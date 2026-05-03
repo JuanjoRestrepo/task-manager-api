289cbb28f074cd5685a5b96acd457cda583564ef
Juan Jose Restrepo Rosero <restrepojuanjo@gmail.com>
2026-05-02 21:40:55 -0500

    fix(prisma): remove adapter-pg to fix P2022 enum bug in jest tests

--- src/tests/jest.teardown.ts
+++ src/tests/jest.teardown.ts
@@ -1,6 +1,5 @@
-import { prisma, pool } from '../persistence/prisma';
+import { prisma } from '../persistence/prisma';
 
 export default async function globalTeardown() {
   await prisma.$disconnect();
-  await pool.end();
 }
